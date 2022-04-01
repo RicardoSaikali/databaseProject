13ed4555085c67f155ddcfcdc7c041242451769c
Ricar <ricardo_saikali@hotmail.com>
2022-04-01 18:10:07 -0400

    connect to db

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,3 +1,4 @@
+//UI imports
 import java.awt.FlowLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -6,9 +7,45 @@ import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.SwingUtilities;
 import javax.swing.ButtonGroup;
+
+//databse imports
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+//other imports
+
 public class main {
     public static void main(String s[]) {
-        createUI();
+
+        /*
+        example:
+        String user = "mzjycxzivsmkni";
+        String pass = "e2de58153c0f251dc70bd1de7544284d80d0032ea323d52bf512ab5f5d93b828";
+        String LINK = "jdbc:postgresql://ec2-52-73-155-171.compute-1.amazonaws.com:5432/dc2qa16v4lv078";
+        run with java  -cp ".\postgresql-42.3.3.jar;.\" main 
+        */
+
+        String user = "mzjycxzivsmkni";
+        String pass = "e2de58153c0f251dc70bd1de7544284d80d0032ea323d52bf512ab5f5d93b828";
+        String LINK = "jdbc:postgresql://ec2-52-73-155-171.compute-1.amazonaws.com:5432/dc2qa16v4lv078";
+
+
+        try (Connection conn = DriverManager.getConnection(LINK,user,pass)) {
+
+            if (conn != null) {
+                System.out.println("Connected to the database!");
+            } else {
+                System.out.println("Failed to make connection!");
+            }
+
+        } catch (SQLException e) {
+            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // createUI();
+
     }
 
     public static void createUI(){
