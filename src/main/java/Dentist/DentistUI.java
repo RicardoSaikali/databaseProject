447a010ca65476117a0241114015dc2866d680f6
S Stella <sstella2001@gmail.com>
2022-04-01 17:14:16 -0400

    Created DentistUI

---
+++ src/main/java/Dentist/DentistUI.java
@@ -0,0 +1,27 @@
+import java.awt.FlowLayout;
+import javax.swing.*;
+
+public class DentistUI {
+    public static void main(String s[]) {
+        JFrame frame = new JFrame("Dentist");
+
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+        JLabel label = new JLabel("Patient ID:");
+
+        JTextField PatientID = new JTextField();
+
+        JButton button1 = new JButton();
+        button1.setText("Retrieve Patient Information");
+
+        panel.add(label);
+        panel.add(label);
+        panel.add(button1);
+        frame.add(panel);
+        frame.setSize(500, 500);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
