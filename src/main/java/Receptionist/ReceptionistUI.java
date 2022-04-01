3527671bdea53b86632c37f27d90572cce311428
Ricar <ricardo_saikali@hotmail.com>
2022-04-01 19:07:52 -0400

    clean up indents

--- src/main/java/Receptionist/ReceptionistUI.java
+++ src/main/java/Receptionist/ReceptionistUI.java
@@ -4,33 +4,34 @@ import java.awt.FlowLayout;
 import javax.swing.*;
 
 public class ReceptionistUI {
-    public ReceptionistUI(){
-      createUI();
+    public ReceptionistUI() {
+        createUI();
     }
-    private void createUI(){
 
-      // JPanel panel = new JPanel();
-      // panel.setLayout(new FlowLayout());
-      // JLabel label = new JLabel("Select Action:");
-      //
-      // JButton button1 = new JButton();
-      // button1.setText("Add Patient Information");
-      //
-      // JButton button2 = new JButton();
-      // button2.setText("Edit Patient Information");
-      //
-      // JButton button3 = new JButton();
-      // button3.setText("Set Patient Appointment");
-      //
-      // panel.add(label);
-      // panel.add(button1);
-      // panel.add(button2);
-      // panel.add(button3);
-      // frame.add(panel);
-      // frame.setSize(500, 500);
-      // frame.setLocationRelativeTo(null);
-      // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      // frame.setVisible(true);
+    private void createUI() {
+
+        // JPanel panel = new JPanel();
+        // panel.setLayout(new FlowLayout());
+        // JLabel label = new JLabel("Select Action:");
+        //
+        // JButton button1 = new JButton();
+        // button1.setText("Add Patient Information");
+        //
+        // JButton button2 = new JButton();
+        // button2.setText("Edit Patient Information");
+        //
+        // JButton button3 = new JButton();
+        // button3.setText("Set Patient Appointment");
+        //
+        // panel.add(label);
+        // panel.add(button1);
+        // panel.add(button2);
+        // panel.add(button3);
+        // frame.add(panel);
+        // frame.setSize(500, 500);
+        // frame.setLocationRelativeTo(null);
+        // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // frame.setVisible(true);
 
     }
 }
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -21,19 +21,20 @@ public class main {
     public static void main(String s[]) {
 
         /*
-        example:
-        String user = "mzjycxzivsmkni";
-        String pass = "e2de58153c0f251dc70bd1de7544284d80d0032ea323d52bf512ab5f5d93b828";
-        String LINK = "jdbc:postgresql://ec2-52-73-155-171.compute-1.amazonaws.com:5432/dc2qa16v4lv078";
-        run with java -cp ".\postgresql-42.3.3.jar;.\" main
-                */
+         * example:
+         * String user = "mzjycxzivsmkni";
+         * String pass =
+         * "e2de58153c0f251dc70bd1de7544284d80d0032ea323d52bf512ab5f5d93b828";
+         * String LINK =
+         * "jdbc:postgresql://ec2-52-73-155-171.compute-1.amazonaws.com:5432/dc2qa16v4lv078";
+         * run with java -cp ".\postgresql-42.3.3.jar;.\" main
+         */
 
         String user = "mzjycxzivsmkni";
         String pass = "e2de58153c0f251dc70bd1de7544284d80d0032ea323d52bf512ab5f5d93b828";
         String LINK = "jdbc:postgresql://ec2-52-73-155-171.compute-1.amazonaws.com:5432/dc2qa16v4lv078";
 
-
-        try (Connection conn = DriverManager.getConnection(LINK,user,pass)) {
+        try (Connection conn = DriverManager.getConnection(LINK, user, pass)) {
 
             if (conn != null) {
                 System.out.println("Connected to the database!");
@@ -50,56 +51,52 @@ public class main {
 
     }
 
-    public static void createUI(){
-      JFrame f = new JFrame("Main");
+    public static void createUI() {
+        JFrame f = new JFrame("Main");
 
-      JLabel label = new JLabel("Select:");
-      JButton select = new JButton("OK");
-      JRadioButton r1=new JRadioButton("Receptionist");
-      JRadioButton r2=new JRadioButton("Dentist");
-      JRadioButton r3=new JRadioButton("User");
+        JLabel label = new JLabel("Select:");
+        JButton select = new JButton("OK");
+        JRadioButton r1 = new JRadioButton("Receptionist");
+        JRadioButton r2 = new JRadioButton("Dentist");
+        JRadioButton r3 = new JRadioButton("User");
 
-      ButtonGroup bg = new ButtonGroup();
-      bg.add(r1);
-      bg.add(r2);
-      bg.add(r3);
+        ButtonGroup bg = new ButtonGroup();
+        bg.add(r1);
+        bg.add(r2);
+        bg.add(r3);
 
-      JPanel radiopanel = new JPanel();
-      radiopanel.add(label);
-      radiopanel.add(r1);
-      radiopanel.add(r2);
-      radiopanel.add(r3);
-      radiopanel.add(select);
-      f.getContentPane().add(radiopanel);
-      radiopanel.setBounds(200,100,100,200);
-      radiopanel.setOpaque(false);
+        JPanel radiopanel = new JPanel();
+        radiopanel.add(label);
+        radiopanel.add(r1);
+        radiopanel.add(r2);
+        radiopanel.add(r3);
+        radiopanel.add(select);
+        f.getContentPane().add(radiopanel);
+        radiopanel.setBounds(200, 100, 100, 200);
+        radiopanel.setOpaque(false);
 
+        f.setLayout(null);
+        f.setSize(500, 500);
+        f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        f.setLocationRelativeTo(null);
+        f.setVisible(true);
+        select.addActionListener(new ActionListener() {
 
-      f.setLayout(null);
-      f.setSize(500,500);
-      f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-      f.setLocationRelativeTo(null);
-      f.setVisible(true);
-      select.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (r1.isSelected()) {
+                    System.out.println("Receptionist");
+                    radiopanel.setVisible(false);
+                    ReceptionistUI receptionist = new ReceptionistUI();
+                } else if (r2.isSelected()) {
+                    System.out.println("Dentist");
+                    radiopanel.setVisible(false);
 
-        public void actionPerformed(ActionEvent e) {
-          if (r1.isSelected()){
-            System.out.println("Receptionist");
-            radiopanel.setVisible(false);
-            ReceptionistUI receptionist = new ReceptionistUI();
-          }
-          else if (r2.isSelected()){
-            System.out.println("Dentist");
-            radiopanel.setVisible(false);
-
-          }
-          else if (r3.isSelected()){
-            System.out.println("User");
-            radiopanel.setVisible(false);
-          }
-        }
-      });
+                } else if (r3.isSelected()) {
+                    System.out.println("User");
+                    radiopanel.setVisible(false);
+                }
+            }
+        });
     }
-
 
 }
