63296ff0d3480d5704f6e4bc60c722e43d418bba
Joseph Abonasara <jabon028@uottawa.ca>
2022-04-03 14:09:07 -0400

    push

--- src/main/java/Receptionist/Receptionist.java
+++ src/main/java/Receptionist/Receptionist.java
@@ -33,8 +33,11 @@ public class Receptionist {
     private static int contactInformationId;
     private static int addressId;
     private static int userId;
+    private static int appointmentId;
     private static int patientId;
     private static Statement statement;
+    private static String[] rooms = new String[] {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+
 
     
     
@@ -90,7 +93,7 @@ public class Receptionist {
         return;
     }
     
-    public boolean isReceptionist(int id){
+    public boolean isReceptionist(Connection conn, int id){
         try {
             //Get contact information id 
             preparedStatement = conn.prepareStatement("SELECT employee_role FROM public.employee WHERE employee_id="+id);
@@ -214,52 +217,87 @@ public class Receptionist {
         try {
             //Get contact information id 
             scanner = new Scanner(System.in);
-            System.out.println("Please enter the SSN of the patient:");
-            ssn = Integer.parseInt(scanner.nextLine());
-            
-            preparedStatement = conn.prepareStatement("SELECT * FROM public.user, public.address, public.contactinformation WHERE SSN="+ ssn+ "and public.user.address_id=public.address.address_id and public.user.contactinfo_id=public.contactinformation.contactinfo_id");
+            System.out.println("Please enter the Branch ID:");
+            int branch = Integer.parseInt(scanner.nextLine());
+            System.out.println("Please enter the Patient ID:");
+            int patientId = Integer.parseInt(scanner.nextLine());
+            System.out.println("Please enter the date (yyyy-mm-dd)");
+            String date = scanner.nextLine();
+            System.out.println("Please enter start time (in military time):");
+            String startime = scanner.nextLine();
+            System.out.println("Please enter end time (in military time):");
+            String endtime = scanner.nextLine();
+            preparedStatement = conn.prepareStatement("SELECT roomassigned FROM public.appointment WHERE date='"+ date+ "' and starttime='" +startime+"'");
             resultSet = preparedStatement.executeQuery();
             System.out.println("------------------------------------------------------------------------------------------------------------");
+            System.out.println("The following rooms are taken at that time:");
+            ArrayList<String> roomsTaken = new ArrayList<String>();
             while (resultSet.next()){ 
-                System.out.println("firstname: " +resultSet.getString("firstname") +",\nmiddlename: "+resultSet.getString("middlename")+ ",\nlastname: " +resultSet.getString("lastname")+ ",\ngender: " +resultSet.getString("gender") + ",\nssn: "+resultSet.getInt("ssn")+",\ndatebirth: "+ resultSet.getDate("datebirth")+
-                ",\napartmentnumber: "+ resultSet.getInt("apartmentnumber")+ ",\nstreetnumber: "+resultSet.getInt("streetnumber")+ ",\nstreet: "+resultSet.getString("street")+ ",\ncity: "+resultSet.getString("city") + ",\nprovince: "+resultSet.getString("province")+ ",\npostalcode: "+resultSet.getString("postalcode") + ",\nemail: "+resultSet.getString("email")
-                + ",\nphonenumber: "+resultSet.getString("phonenumber"));
-                
-                addressId = resultSet.getInt("address_id");
-                userId = resultSet.getInt("user_id");
-                contactInformationId = resultSet.getInt("contactinfo_id");
+                System.out.println(resultSet.getString("roomassigned"));
+                roomsTaken.add(resultSet.getString("roomassigned"));
             }
+            System.out.println("Which means the following rooms are available at that time:");
+            for(int i=0;i<rooms.length;i++){
+                if(!roomsTaken.contains(rooms[i])){
+                    System.out.println(rooms[i]);
+                }
+            }
+            boolean flag = true;
+            String room="";
+            while(flag){
+                System.out.println("Please enter which available room you would like to book:");
+                room = scanner.nextLine();
+                if(Arrays.asList(rooms).contains(room) && !roomsTaken.contains(room)) flag = false;
+                else{
+                    System.out.println("Invalid Entry");
+                }
+            }
+            System.out.println("Are there any notes you like to add:");
+            String notes = scanner.nextLine();
+            flag=true;
+            //get all busy dentists
+            preparedStatement = conn.prepareStatement("SELECT employee_id FROM public.appointment WHERE date='"+ date+ "' and starttime='" +startime+"'");
+            resultSet = preparedStatement.executeQuery();
+            ArrayList<Integer> busyDentists = new ArrayList<Integer>();
             System.out.println("------------------------------------------------------------------------------------------------------------");
-           
-            System.out.println("Please enter the field you would like to update :");
-            String field = scanner.nextLine();
-            
-            System.out.println("What would you like to change it to :");
-            Object value;            
-            if(field.equals("apartmentnumber") || field.equals("streetnumber")|| field.equals("ssn")){
-                value = Integer.parseInt(scanner.nextLine());
-            }else{
-                value = scanner.nextLine();
+            System.out.println("The following Dentists are busy at this time:");
+            while (resultSet.next()){ 
+                busyDentists.add(resultSet.getInt("employee_id"));
+                System.out.println(resultSet.getInt("employee_id"));
             }
-            
-            String table ="";
-            int id=0;
-            String type="";
-            if(field.equals("email")||field.equals("phonenumber")){
-                table = "public.contactInformation";
-                id = contactInformationId;
-                type="contactinfo_id";
-            }else if(field.equals("apartmentnumber")||field.equals("streetnumber")||field.equals("street")||field.equals("city")||field.equals("province")||field.equals("postalcode")){
-                table = "public.address";
-                id = addressId;
-                type="address_id";
-            }else if(field.equals("firstname")||field.equals("middlename")||field.equals("lastname")||field.equals("gender")||field.equals("ssn")||field.equals("datebirth")){
-                table = "public.user";
-                type="user_id";
-                id = userId;
+            //get all dentists
+            preparedStatement = conn.prepareStatement("SELECT employee_id FROM public.employee WHERE employee_role = 'Dentist' and branch_id="+branch);
+            resultSet = preparedStatement.executeQuery();
+            ArrayList<Integer> dentists = new ArrayList<Integer>();
+            System.out.println("------------------------------------------------------------------------------------------------------------");
+            System.out.println("But, the following Dentists are available:");
+            while (resultSet.next()){ 
+                dentists.add(resultSet.getInt("employee_id"));
+                System.out.println(resultSet.getInt("employee_id"));
             }
+
+            int dentistId=0;;
+            while(flag){
+                System.out.println("Please enter the employee_id of the Dentist");
+                dentistId = Integer.parseInt(scanner.nextLine());
+                
+                if(!dentists.contains(dentistId)){
+                    System.out.println("That is not a valid employee_id");
+                }
+                else if(busyDentists.contains(dentistId)){
+                    System.out.println("This Dentist is busy at that time");
+                } 
+                else if(!busyDentists.contains(dentistId) && dentists.contains(dentistId)){
+                    flag=false;
+                }
+            }
+            preparedStatement = conn.prepareStatement("SELECT max(appointment_id) FROM public.appointment");
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) appointmentId = resultSet.getInt("max");
+            appointmentId++;
             // preparedStatement = conn.prepareStatement("UPDATE "+ table + "SET " + field +" = " +value +" WHERE SSN = "+ssn );
-            String sql = "UPDATE "+ table + " SET " + field +" = '" +value +"' WHERE " + type +" = " +id;
+            String appointmentInfo = appointmentId +", '"+ date+"', '"+startime+"', '"+endtime+"',"+ null +",'" + room+"', '"+notes+"', "+patientId+", "+ dentistId;
+            String sql = "INSERT INTO public.appointment values ("+appointmentInfo + ")";
             statement = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
             statement.addBatch(sql);
             statement.executeBatch();
