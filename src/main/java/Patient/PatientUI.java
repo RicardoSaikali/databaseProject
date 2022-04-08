1a4ff04bfeda2368526de310f01251a9e9e2cfe9
S Stella <sstella2001@gmail.com>
2022-04-08 07:44:45 -0400

    aids

--- src/main/java/Patient/PatientUI.java
+++ src/main/java/Patient/PatientUI.java
@@ -13,6 +13,7 @@ import Patient.Patient;
 public class PatientUI{
 
   public Patient patient;
+  public HashMap<String, String> patientInfo;
 
   public PatientUI(JFrame aJFrame){
     createUI(aJFrame);
@@ -49,6 +50,7 @@ public class PatientUI{
               if (isInteger(s)) {
                   patient = new Patient();
                   if (patient.isPatient(Integer.parseInt(s))) {
+                      patientInfo = patient.getPatientInfo(Integer.parseInt(s));
                       constructMainPatientUI(f);
                   } else {
                       JLabel errorLabel = new JLabel("Wrong ID");
@@ -75,39 +77,42 @@ public class PatientUI{
       f.getContentPane().removeAll();
       SwingUtilities.updateComponentTreeUI(f);
 
-      // Receptionist receptionist = new Receptionist();
-      JPanel p = new JPanel();
-      p.setBounds(0, 100, f.getWidth(), 40);
-      // p.setBackground(Color.red);
-      JLabel label = new JLabel("Select function:");
-      label.setBounds(200, 0, 100, 40);
+    String[] dataKeys = {"firstname", "middlename", "lastname", "datebirth", "gender", "ssn", "streetaddress", "city", "province", "postalcode", "email", "phonenumber", "insurancenumber"};
+    String[] associatedText = {"First Name: ", "Middle Name: ", "Last Name: ", "D.O.B: ", "Gender: ", "SSN: ", "Street: ", "City: ", "Province: ", "Postal Code: ", "Email: ", "Phone Number: ", "Insurance: "};
+    JLabel tmpLabel;
+    for(int j = 0; j<dataKeys.length; j++){
+        //set labels for this panel
+        tmpLabel = new JLabel(associatedText[j]+ patientInfo.get(dataKeys[j]) + "  ");
+        tmpLabel.setBounds(0, j*30 + 10, 200, 30);
+        f.add(tmpLabel);
+    }
+      //label.setBounds(200, 0, 100, 40);
 
-      JPanel p2 = new JPanel();
-      p2.setBounds(0, 140, f.getWidth(), 40);
-      // p2.setBackground(Color.blue);
-      JButton btn = new JButton("Insert new patient");
-      btn.setPreferredSize(new Dimension(150, 30));
+      // JPanel p2 = new JPanel();
+      // p2.setBounds(0, 140, f.getWidth(), 40);
+      // // p2.setBackground(Color.blue);
+      // JButton btn = new JButton("Insert new patient");
+      // btn.setPreferredSize(new Dimension(150, 30));
 
-      JPanel p3 = new JPanel();
-      p3.setBounds(0, 180, f.getWidth(), 40);
-      // p3.setBackground(Color.green);
-      JButton btn2 = new JButton("Edit exsiting patient");
-      btn2.setPreferredSize(new Dimension(150, 30));
+      // JPanel p3 = new JPanel();
+      // p3.setBounds(0, 180, f.getWidth(), 40);
+      // // p3.setBackground(Color.green);
+      // JButton btn2 = new JButton("Edit exsiting patient");
+      // btn2.setPreferredSize(new Dimension(150, 30));
 
-      JPanel p4 = new JPanel();
-      p4.setBounds(0, 220, f.getWidth(), 40);
-      // p4.setBackground(Color.yellow);
-      JButton btn3 = new JButton("Set Appointment");
-      btn3.setPreferredSize(new Dimension(150, 30));
+      // JPanel p4 = new JPanel();
+      // p4.setBounds(0, 220, f.getWidth(), 40);
+      // // p4.setBackground(Color.yellow);
+      // JButton btn3 = new JButton("Set Appointment");
+      // btn3.setPreferredSize(new Dimension(150, 30));
 
-      p4.add(btn3);
-      p3.add(btn2);
-      p2.add(btn);
-      p.add(label);
-      f.add(p);
-      f.add(p2);
-      f.add(p3);
-      f.add(p4);
+      // p4.add(btn3);
+      // p3.add(btn2);
+      // p2.add(btn);
+
+      // f.add(p2);
+      // f.add(p3);
+      // f.add(p4);
       // btn2.addActionListener(new ActionListener() {
 
       //     public void actionPerformed(ActionEvent e) {
